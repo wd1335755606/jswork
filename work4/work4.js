9a91040a80ca7d10c75ec54c4858e0601cbfa747
wd1335755606 <55181374+wd1335755606@users.noreply.github.com>
2019-10-12 12:59:16 +0800

    Update work4.js

--- work4/work4.js
+++ work4/work4.js
@@ -1,6 +1,6 @@
 document.write("<p>金字塔</p>")
 let level = prompt('请设置金字塔的层数')
-level = parseFloat(level)&&Number(level)
+level = parseFloat(level)&&Number(level)
 if(isNaN(level)){
     alert('金字塔的层数是数字')
 }
